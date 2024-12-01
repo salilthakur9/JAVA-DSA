79e1abf1404c18889a46f2a07da655e6385039a9
Salil Thakur <salilthakur09@gmail.com>
2024-12-02 00:05:59 +0530

    output in java

--- JavaBasics.java
+++ JavaBasics.java
@@ -1,5 +1,20 @@
-public class JavaBasics {
+/*public class JavaBasics {
     public static void main(String[]args){
+        System.out.print("Salil Thakur\n");
         System.out.println("Salil Thakur");
     }
-}  
+}   */
+
+
+/*public class JavaBasics{
+    public static void main(String[]args){
+        int a= 50;
+        int b=10;
+        System.out.println(a);
+        System.out.println(b);
+        String name= "Salil Thakur";
+        System.out.println(name);
+        a=b;
+        System.out.println(a);
+    }
+}*/
