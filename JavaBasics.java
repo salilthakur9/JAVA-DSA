03febca2f06a882ab9bd41e72c821535eff32b7c
Salil Thakur <salilthakur09@gmail.com>
2024-12-02 13:22:03 +0530

    else if

--- JavaBasics.java
+++ JavaBasics.java
@@ -113,3 +113,22 @@ public class JavaBasics {
         }
     }
 }*/
+
+
+/*import java.util.Scanner;
+public class JavaBasics{
+    public static void main(String args[]){
+        Scanner s=new Scanner(System.in);
+        int num1 = s.nextInt();
+        int num2=s.nextInt();
+        int num3=s.nextInt();
+
+        if(num1>=num2 && num1>=num3){
+            System.out.println(num1+" is the largest");
+        }else if(num2>=1 && num2>=num3){
+            System.out.println(num2+" is the largest");
+        }else{
+            System.out.println(num3+" is the largest");
+        }
+    }
+}*/
