94ac0c1ca65d31998860174a7ab204b79cd7c2c0
Salil Thakur <salilthakur09@gmail.com>
2024-12-02 11:57:30 +0530

    operators

--- JavaBasics.java
+++ JavaBasics.java
@@ -76,3 +76,12 @@ public class JavaBasics{
         System.out.println(num);
     }
 }*/
+
+
+/*public class JavaBasics{
+    public static void main(String args[]){
+        char a='a';
+        char b='b';
+        System.out.println(a-b);  
+    }
+}*/
