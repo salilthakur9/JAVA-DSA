844cf542d45ffa007b8b47188628c308e948b041
Salil Thakur <salilthakur09@gmail.com>
2024-12-04 17:25:37 +0530

    similar one

--- JavaBasics.java
+++ JavaBasics.java
@@ -351,3 +351,33 @@ public class JavaBasics{
         }
     }
 }*/
+
+
+
+/*import java.util.*;
+public class JavaBasics{
+    public static int findKey(int arr[], int key){
+        for(int i=0;i<arr.length;i++){
+            if(arr[i]==key)
+            return i;
+        }
+        return -1;
+    }
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        int n=sc.nextInt();
+        int arr[]=new int[10];
+        for(int i=0;i<n;i++){
+            arr[i]=sc.nextInt();
+        }
+        int key=sc.nextInt();
+
+        int index=findKey(arr, key);
+        if(index==-1){
+            System.out.println("Key not found!");
+        }else{
+            System.out.println("key found at index "+index);
+        }
+        sc.close();
+    }
+}*/
