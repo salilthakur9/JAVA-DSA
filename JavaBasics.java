0545c7159937004a0461876acb26aab476ff849c
Salil Thakur <salilthakur09@gmail.com>
2024-12-04 16:45:22 +0530

    upto arrays

--- JavaBasics.java
+++ JavaBasics.java
@@ -310,3 +310,21 @@ public class JavaBasics{
 
         sc.close();
     }*/
+
+
+/*import java.util.Scanner;
+public class JavaBasics{
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        int arr[]= new int[100];
+
+        arr[0]=sc.nextInt();
+        arr[1]=sc.nextInt();
+        arr[2]=sc.nextInt();
+
+        System.out.println("{"+arr[0]+", "+arr[1]+", "+arr[2]+"}");
+
+        sc.close();
+
+    }
+}*/
