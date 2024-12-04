044fb658b498cba6226efda6047245a3a2232410
Salil Thakur <salilthakur09@gmail.com>
2024-12-04 17:35:57 +0530

    teacher assign

--- JavaBasics.java
+++ JavaBasics.java
@@ -381,3 +381,39 @@ public class JavaBasics{
         sc.close();
     }
 }*/
+
+
+/*import java.util.Scanner;
+public class JavaBasics{
+    public static void main(String args[]){
+        Scanner s=new Scanner(System.in);
+        int number=s.nextInt();
+        if(number<=0){
+            System.out.println("zero and negative numbers are not allowed!");
+        }else{
+            if(number%2==0){
+                System.out.println("number is divisible by 2 (even)");
+            }else{
+                System.out.println("number is not divisible by 2 (Odd)");
+            }
+        }
+    }
+}*/
+
+
+/*import java.util.Scanner;
+public class JavaBasics{
+    public static void main(String args[]){
+        Scanner sc=new Scanner(System.in);
+        int num1=sc.nextInt(),num2=sc.nextInt(),num3=sc.nextInt();
+
+        if(num1>=num2 && num1>=num3){
+            System.out.println(num1+" is greatest!");
+        }else if(num2>=num1 && num2>=num3){
+            System.out.println(num2+" is greatest!");
+        }else{
+            System.out.println(num3+" is greatest!");
+        }
+        sc.close();
+    }
+}*/
