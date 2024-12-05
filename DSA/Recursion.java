0e8772fbe64a84dfe9b80c3b619f1da4f0bfb082
Salil Thakur <salilthakur09@gmail.com>
2024-12-05 14:46:03 +0530

    Recursion1

---
+++ DSA/Recursion.java
@@ -0,0 +1,14 @@
+/*package DSA;
+import java.util.Scanner;
+public class Recursion{
+    public static void recursion(int i,int n){
+            if(i>n)return;
+            System.out.println("Salil");
+            recursion(i+1, n);
+        }
+        public static void main(String[] args) {
+            Scanner sc = new Scanner(System.in);
+            int n=sc.nextInt();
+            recursion(1,n);
+    }
+}*/
