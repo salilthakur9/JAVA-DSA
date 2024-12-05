5cda87de501526e758ce849bf89c43561d7f62e5
Salil Thakur <salilthakur09@gmail.com>
2024-12-05 14:54:51 +0530

    recursion2

--- DSA/Recursion.java
+++ DSA/Recursion.java
@@ -12,3 +12,18 @@ public class Recursion{
             recursion(1,n);
     }
 }*/
+
+
+/*import java.util.Scanner;
+public class Recursion{
+    public static void recursion(int i,int n){
+        if(i>n)return;
+        System.out.println(i);
+        recursion(i+1,n);
+    }
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        int n=sc.nextInt();
+        recursion(1,n);
+    }
+}*/
