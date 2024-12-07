af196835cbba61e65ad24bd8e3edd5abe1805e68
Salil Thakur <salilthakur09@gmail.com>
2024-12-07 16:14:13 +0530

    recursion 7

--- DSA/Recursion.java
+++ DSA/Recursion.java
@@ -89,3 +89,18 @@ public class Recursion{
         System.out.println(sum(num));
     }
 }*/
+
+
+//7
+/*import java.util.Scanner;
+public class Recursion{
+    public static int fact(int n){
+        if(n==1)return 1;
+        return n*fact(n-1);
+    }
+    public static void main(String args[]){
+        Scanner sc= new Scanner(System.in);
+        int num=sc.nextInt();
+        System.out.println(fact(num));
+    }
+}*/
