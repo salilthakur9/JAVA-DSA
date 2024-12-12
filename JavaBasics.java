228adeae2c53754d838a0150a8bd3004f4d5308a
Salil Thakur <salilthakur09@gmail.com>
2024-12-12 17:36:39 +0530

    class

--- JavaBasics.java
+++ JavaBasics.java
@@ -694,3 +694,30 @@ public class JavaBasics {
 
 
 
+/*public class JavaBasics {
+    public static void main(String args[]){
+        for(int i=1;i<=3;i++){
+            for(int j=1;j<=2;j++){
+                System.out.println("i= "+i+" j= "+j);
+            }
+        }
+    }
+}*/
+
+
+
+/*import java.util.Scanner;
+public class JavaBasics{
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a number: ");
+        int number = sc.nextInt();
+
+        for (int i = 1; i <= 1; i++) {
+            for (int j = 1; j <= 10; j++) {
+                System.out.println(number + " x " + j + " = " + (number * j));
+            }
+            System.out.println();
+        }
+    }
+}*/
