33157741b5fa0c4414d781b971458b93efa410f3
Salil Thakur <salilthakur09@gmail.com>
2025-01-14 20:14:25 +0530

    class

--- JavaBasics.java
+++ JavaBasics.java
@@ -1078,3 +1078,17 @@ public class JavaBasics {
 
 
 
+/*public class JavaBasics{
+    public static void new_pair(int num[]){
+        for(int i=0;i<num.length;i++){
+            int curr=num[i];
+            for(int j=i+1;j<num.length;j++){
+                System.out.print("("+num[i]+", "+num[j]+") ");
+            }System.out.println();
+        }
+    }
+    public static void main(String args[]){
+        int num[]={1,2,3,4,5,6,7,8,9,10};
+        new_pair(num);
+    }
+}*/
