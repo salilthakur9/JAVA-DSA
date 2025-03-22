4aed1a436424612915eb6f0b1e9003b08402619e
Salil Thakur <salilthakur09@gmail.com>
2025-03-22 16:59:06 +0530

    class

--- linkList/linkListClass.java
+++ linkList/linkListClass.java
@@ -930,3 +930,189 @@ public class linkListClass {
 	
 	}
 }*/
+
+
+
+/*public class linkListClass {
+	class Node
+	{  
+		int rollno;
+		Node prev;
+		Node next;
+
+		public Node(int rollno)
+		{  
+			this.rollno=rollno;
+		}  
+	}  
+	 
+	Node head=null;
+	Node tail=null;  
+		  
+	public void addNode(int data)
+	{  
+		Node newnode = new Node(data);  
+		  
+		if(head==null)
+		{
+		    head=tail=newnode;  
+		    
+		    head.prev=null;  
+		    
+		    tail.next=null;  
+		}
+		else
+		{  
+			tail.next=newnode;  
+		    
+		    newnode.prev=tail;  
+		    
+		    tail=newnode;  
+		     
+		    tail.next=null;
+		}  
+	}
+	
+	public void addNodeAtStart(int roll)
+	{
+		Node newnode=new Node(roll);
+		newnode.prev=null;
+		newnode.next=head;
+		head.prev=newnode;
+		head=newnode;
+	}
+	
+	public void insert(int key,int data)
+	{
+		Node newnode,current=head;
+		int flag=0;
+		while (current!=null)
+		{
+			if (current.rollno==key)
+			{
+				flag=1;
+				newnode=new Node(data);
+				newnode.next=current.next;
+				newnode.prev=current;
+				current.next.prev=newnode;
+				current.next=newnode;
+				current=newnode;
+			}
+			current=current.next;
+		}
+		if (flag==0)
+		{
+			System.out.println("Node not found");
+		}
+	}
+	
+	public void display()
+	{   
+		Node current=head;  
+		
+		if(head==null)
+		{  
+			System.out.println("List is empty");  
+			return;  
+        }
+		System.out.println("Nodes of doubly linked list: ");  
+		
+		while(current!=null)
+		{    
+			System.out.print(current.rollno+" ");  
+		    current=current.next;
+        }
+		System.out.println();
+	}
+	
+	public void reversedisplay()
+	{  
+		Node current=tail;  
+			
+		if(tail==null)
+		{  
+			System.out.println("List is empty");  
+			return;
+	    }
+		System.out.println("Nodes of doubly linked list: ");  
+			
+		while(current!= null)
+		{  
+			System.out.print(current.rollno+" ");  
+			current=current.prev;
+		}
+		System.out.println();
+	}
+	
+	public void countNodes()
+	{
+		int count=0;
+		if (tail==null)
+		{
+			System.out.println("Empty double linked list");
+		}
+		else
+		{
+			Node current=tail;
+			while (current!=null)
+			{
+				count++;
+				current=current.prev;
+			}
+			System.out.println("Number of nodes are "+count);
+		}
+	}
+
+	public void search(int key)
+	{
+		Node current;
+		int flag=0,pos=1;
+		
+		current=head;
+		while (current!=null)
+		{
+			if (current.rollno==key)
+			{
+				flag=1;
+				break;
+			}
+			current=current.next;
+			pos++;
+		}
+		if (flag==1)
+		{
+			System.out.println("Element found in the linked list at "
+		+pos+" position");
+		}
+		else
+		{
+			System.out.println("Element not found in the linked list");
+		}
+	}
+	
+	public static void main(String[] args)
+	{  
+		linkListClass dl = new linkListClass();  
+		dl.display();
+		dl.reversedisplay();
+		
+		dl.addNode(10);
+		dl.addNode(35);
+		dl.addNode(57);
+		dl.addNode(24);
+	
+		dl.display();
+		dl.reversedisplay();
+				
+		dl.addNodeAtStart(77);
+		dl.display();
+		
+		dl.insert(57,92);
+		dl.display();
+		dl.reversedisplay();
+		dl.countNodes();
+		
+		dl.search(57);
+		dl.search(100);
+	}  
+}*/
