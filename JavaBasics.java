78da027fc590d705add0164ecb38d86499e6598e
Salil Thakur <salilthakur09@gmail.com>
2025-03-22 18:19:22 +0530

    practice

--- JavaBasics.java
+++ JavaBasics.java
@@ -2576,3 +2576,42 @@ public class JavaBasics{
 }*/
 
 
+
+/*public class JavaBasics{
+    public static void rotate(int[][] arr){
+        for(int i=0;i<arr.length;i++){
+            for(int j=i;j<arr.length;j++){
+                int temp=arr[i][j];
+                arr[i][j]=arr[j][i];
+                arr[j][i]=temp;
+            }
+        }
+
+        for(int i=0;i<arr.length;i++){
+            for(int j=0;j<arr.length/2;j++){
+                int temp=arr[i][j];
+                arr[i][j]=arr[i][arr.length-j-1];
+                arr[i][arr.length-j-1]=temp;
+            }
+        }
+    }
+    public static void display(int[][] arr){
+        for(int[] row:arr){
+            for(int val:row){
+                System.out.print(val+" ");
+            }System.out.println();
+        }
+    }
+    public static void main(String args[]){
+        int[][] arr={
+            {1,2,3},
+            {4,5,6},
+            {7,8,9}
+        };
+        rotate(arr);
+        display(arr);
+    }
+}*/
+
+
+
