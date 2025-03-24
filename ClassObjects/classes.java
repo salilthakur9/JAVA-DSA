ca6ce5b164af0a62fd326af3eaad63cfdf48456e
Salil Thakur <salilthakur09@gmail.com>
2025-03-24 18:15:11 +0530

    practice classes

---
+++ ClassObjects/classes.java
@@ -0,0 +1,21 @@
+/*public class classes{
+    public static class car{
+        String brand;
+        int date;
+    
+        public void display(){
+            System.out.println("This is car brand: "+brand);
+            System.out.println("This is date: "+date);
+        }
+    }
+    public static void main(String args[]){
+        car c=new car();
+        c.brand="abc";
+        c.date=2025;
+
+        c.display();
+    }
+}*/
+
+
+
