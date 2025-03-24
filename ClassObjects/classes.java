f8fb796ad22249eb1152385fdaa46c8be5ddb3c5
Salil Thakur <salilthakur09@gmail.com>
2025-03-24 18:32:31 +0530

    practice

--- ClassObjects/classes.java
+++ ClassObjects/classes.java
@@ -18,4 +18,35 @@
 }*/
 
 
+/*import java.util.Scanner;
+class student{
+    String name;
+    int rollno;
+    char grade;
 
+    void display(){
+        System.out.println("Student Name: "+name);
+        System.out.println("Student's roll number: "+rollno);
+        System.out.println("Grade Obtained: "+grade);
+    }
+}
+public class classes{
+    public static void main(String args[]){
+        Scanner sc= new Scanner(System.in);
+        System.out.println("Enter name of the student: ");
+        String Name=sc.nextLine();
+        System.out.println("enter roll number of the student: ");
+        int Rollno=sc.nextInt();
+        System.out.println("Enter the grade obtained: ");
+        char Grade=sc.next().charAt(0);
+
+        student st=new student();
+        st.name=Name;
+        st.rollno=Rollno;
+        st.grade=Grade;
+       
+        st.display();
+
+        sc.close();
+    }
+}*/
