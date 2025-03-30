899c24556268473caa0bb7154d30159bdb026e22
Salil Thakur <salilthakur09@gmail.com>
2025-03-30 14:59:31 +0530

    practice

---
+++ Sorting/insertion.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+public class insertion {
+    public static void insertion_sort(int arr[], int n){
+        for(int i=0;i<n;i++){
+            int j=i;
+            while(j>0 && arr[j-1]>arr[j]){
+                int temp=arr[j];
+                arr[j]=arr[j-1];
+                arr[j-1]=temp;
+                j--;
+            }
+        }
+    }
+    public static void main(String args[]){
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        int[] arr=new int[n];
+        for(int i=0;i<n;i++){
+            arr[i]=sc.nextInt();
+        }
+        insertion_sort(arr, n);
+        for(int i=0;i<n;i++){
+            System.out.print(arr[i]+" ");
+        }
+        sc.close();
+    }
+}
