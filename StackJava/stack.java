dc213411eaf4e55f991828986097b71eaa747c4f
Salil Thakur <salilthakur09@gmail.com>
2025-03-31 18:41:39 +0530

    class

--- StackJava/stack.java
+++ StackJava/stack.java
@@ -20,3 +20,29 @@ public class stack {
         sc.close(); 
     }
 }*/
+
+
+
+
+/*import java.util.Scanner;
+import java.util.Stack;
+public class stack {
+    public static void main(String[] args) {
+        String name;
+        Stack<String> st = new Stack<>();
+        System.out.println("Enter the string");
+        Scanner sc = new Scanner(System.in);
+        name = sc.nextLine();
+        String[] nm = name.split("\\s+");
+        for (String str : nm) {
+            st.push(str);
+        }
+        System.out.println("Required string is ");
+        while (!st.isEmpty()) {
+            System.out.print(st.pop() + " ");
+        }
+    }
+}*/
+
+
+
