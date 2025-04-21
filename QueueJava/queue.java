e49a788b4e96ff757e8af8959ff56f1b2d4c0c05
Salil Thakur <salilthakur09@gmail.com>
2025-04-21 18:23:19 +0530

    class

--- QueueJava/queue.java
+++ QueueJava/queue.java
@@ -141,3 +141,26 @@ public class queue
 		}
 	}
 }*/
+
+
+
+/*import java.util.*;
+public class queue {
+    public static void main(String[] args) {
+        Deque<Integer> deque = new ArrayDeque<>();
+
+        deque.addFirst(2);
+        deque.addFirst(1);
+        deque.addLast(3);
+        deque.addLast(4);
+
+        System.out.println("Deque: " + deque);
+        System.out.println("First Element: " + deque.getFirst());
+        System.out.println("Last Element: " + deque.getLast());
+
+        deque.removeFirst();
+        deque.removeLast();
+
+        System.out.println("Deque: " + deque);
+	} 
+}*/
