e7baa99edc36134f4cbe37c1aa7444f1dd0dff35
Salil Thakur <salilthakur09@gmail.com>
2025-05-27 12:55:24 +0530

    practice

--- Trees/tree.java
+++ Trees/tree.java
@@ -363,3 +363,42 @@ public class tree{
         t.nonleafNodes();
     }
 }*/
+
+
+
+import java.util.*;
+class Node{
+    int data;
+    Node left,right;
+
+    Node(int data){
+        this.data=data;
+        left=right=null;
+    }
+}
+
+class tree{
+    static boolean isIdentical(Node r1, Node r2){
+        if(r1==null && r2==null){
+            return true;
+        }
+        if(r1==null || r2==null){
+            return false;
+        }
+        return(r1.data==r2.data) && isIdentical(r1.left, r2.left) && isIdentical(r1.right, r2.right);
+    }
+    public static void main(String args[]){
+        Node r1=new Node(1);
+        r1.left=new Node(2);
+        r1.right=new Node(3);
+        r1.left.left=new Node(4);
+
+        Node r2=new Node(0);
+        r2.left=new Node(2);
+        r2.right=new Node(3);
+        r2.left.left=new Node(4);
+
+        if(isIdentical(r1, r2)){System.out.println("True!!");}
+        else{System.out.println("False!!");}
+    }
+}
