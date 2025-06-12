82a9aacfa2594a49ca837bc80a1b5a703ab7bc86
Salil Thakur <salilthakur09@gmail.com>
2025-06-12 13:58:14 +0530

    hashing int

---
+++ DSA/Hashing.java
@@ -0,0 +1,25 @@
+/*import java.util.Scanner;
+public class Hashing {
+    public static void main(String args[]){
+    Scanner sc=new Scanner(System.in);
+    int n=sc.nextInt();
+    int[] arr=new int[n];
+    for(int i=0;i<n;i++){
+        arr[i]=sc.nextInt();
+    }
+    int[] hash=new int[10];
+    for(int a=0;a<n;a++){
+        hash[arr[a]]+=1;
+    }
+
+    int q=sc.nextInt();
+    while(q-->0){
+        int num=sc.nextInt();
+        System.out.println("ans "+hash[num]);
+    }
+    
+}
+}*/
+
+
+
