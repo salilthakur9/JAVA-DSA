d3d0ab671268f6463e640539d7ee3354e50bc4d4
Salil Thakur <salilthakur09@gmail.com>
2025-07-01 18:39:01 +0530

    class

---
+++ Exception.java
@@ -0,0 +1,23 @@
+/*public class Exception {
+    public static void main(String args[]){
+        int a=10,b=0;
+        int c=a/b;
+        System.out.println("result= "+c);
+    }
+}*/
+
+
+/*class Exception{
+    public static void main(String args[]){
+        int a=10,b=0;
+        try {
+            int c=a/b;
+            System.out.println("result: "+c);
+        } catch (ArithmeticException e) {
+            System.out.println("Division by zero is not allowed");
+        }
+        finally{
+            System.out.println("Program has been executed!");
+        }
+    }
+}*/
