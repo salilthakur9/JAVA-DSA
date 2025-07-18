213f7d02792e651702c06da73c8dd4f5f5bf9801
Salil Thakur <salilthakur09@gmail.com>
2025-07-18 18:01:51 +0530

    practice

--- Exception.java
+++ Exception.java
@@ -606,3 +606,19 @@ public class first{
         }
     }
 }*/
+
+
+
+/*public class Exception{
+    public static void main(String args[]){
+        int a=10,b=0,c;
+        try{
+            c=a/b;
+            System.out.println(c);
+        }catch(ArithmeticException e){
+            System.out.println("Arithmetic Exception found !!");
+        }finally{
+            System.out.println("Final block");
+        }
+    }
+}*/
